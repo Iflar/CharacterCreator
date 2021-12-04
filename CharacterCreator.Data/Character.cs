203bb051878f9b97f9f9bf02ad2b033ba87ecd66
iflar <classicsience@gmail.com>
2021-12-03 19:53:16 -0500

    Character models added

--- CharacterCreator.Data/Character.cs
+++ CharacterCreator.Data/Character.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CharacterCreator.Data
 {
-    class Character
+    public class Character
     {
+        /*
+         requiered models:
+        - detail = everything
+        - create = all but id
+        - edit = all but id
+        - ListItem = Id + Title/Name
+         */
+
+        [Key]
+        public Guid CharacterId { get; set; } //implement relationship to team
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public string Race { get; set; }
+        public int Age { get; set; } 
+
+        
     }
 }
