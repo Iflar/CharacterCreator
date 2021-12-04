5f2bfa3eb01ad9ee19aa881ac4f59d851da5e289
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2021-12-03 19:53:22 -0500

    Team Data Object + Team Models added

---
+++ CharacterCreator.Models/TeamListItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Models
+{
+    public class TeamListItems
+    {
+        public Guid TeamId { get; set; }
+        public string TeamName { get; set; }
+    }
+}
---
+++ CharacterCreator.Services/TeamService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Services
+{
+    public class TeamService
+    {
+        private readonly Guid _teamId;
+
+        public TeamService(Guid teamId)
+        {
+
+        }
+    }
+}
